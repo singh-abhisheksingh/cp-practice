a005a7710e6c80c46fc92b1eae2344ba9c9c52c3
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-09-10 02:42:41 +0530

    September Long Problem 3 with TLE (with recursion)

--- codechef/XORIER.java
+++ codechef/XORIER.java
@@ -7,6 +7,7 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
+    static int c = 0;
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
@@ -18,18 +19,35 @@ class Codechef
 		    for (int i=0; i<N; i++){
 		        arr[i] = sc.nextInt();
 		    }
-		    int c = 0;
-		    for (int i=0; i<N; i++){
-		        int ai = arr[i];
-		        for (int j=i+1; j<N; j++){
-		            int aj = arr[j];
-		            int xor = ai ^ aj;
-		            if((xor>2) && (xor%2==0)){
-		                c++;
-		            }
-		        } 
-		    }
+		    c = 0;
+		    int comb[]=new int[2];
+		    combinations(arr, N, 2, 0, comb, 0);
 		    System.out.println(c);
 		}
 	}
+	
+	static void combinations(int arr[], int n, int r, int index, int comb[], int i)
+    {
+        if (index == r)
+        {
+            int ai = comb[0];
+            int aj = comb[1];
+            if(ai==aj){
+                return;
+            }
+            if((ai%2==0 && aj%2==0) || (ai%2!=0 && aj%2!=0)){
+                if (((ai%4==0 && (ai+2)==aj)||(aj%4==0 && (aj+2)==ai)) || ((ai%4==1 && (ai+2)==aj)||(aj%4==1 && (aj+2)==ai)) ){
+                    return;
+                }
+                c++;
+            }
+            return;
+        }
+        if (i >= n)
+            return;
+        comb[index] = arr[i];
+        combinations(arr, n, r, index+1, comb, i+1);
+        combinations(arr, n, r, index, comb, i+1);
+    }
+    
 }
