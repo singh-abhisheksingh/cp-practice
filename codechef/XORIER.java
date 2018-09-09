7d04705ef74d07cea6b411e57ba7d8dbb99f0f30
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-09-10 02:40:39 +0530

    September Long Problem 3 with TLE

---
+++ codechef/XORIER.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int t=1; t<=T; t++){
+		    int N = sc.nextInt();
+		    int arr[] = new int[N];
+		    for (int i=0; i<N; i++){
+		        arr[i] = sc.nextInt();
+		    }
+		    int c = 0;
+		    for (int i=0; i<N; i++){
+		        int ai = arr[i];
+		        for (int j=i+1; j<N; j++){
+		            int aj = arr[j];
+		            int xor = ai ^ aj;
+		            if((xor>2) && (xor%2==0)){
+		                c++;
+		            }
+		        } 
+		    }
+		    System.out.println(c);
+		}
+	}
+}
