a4d3449a0d70188fea3a88fd7548255ca94f1b9d
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-09-11 15:01:09 +0530

    September Long Problem 3

--- codechef/XORIER.java
+++ codechef/XORIER.java
@@ -7,47 +7,48 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
-    static int c = 0;
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for (int t=1; t<=T; t++){
-		    int N = sc.nextInt();
-		    int arr[] = new int[N];
-		    for (int i=0; i<N; i++){
-		        arr[i] = sc.nextInt();
-		    }
-		    c = 0;
-		    int comb[]=new int[2];
-		    combinations(arr, N, 2, 0, comb, 0);
-		    System.out.println(c);
-		}
-	}
-	
-	static void combinations(int arr[], int n, int r, int index, int comb[], int i)
+    public static void main (String[] args) throws java.lang.Exception
     {
-        if (index == r)
-        {
-            int ai = comb[0];
-            int aj = comb[1];
-            if(ai==aj){
-                return;
-            }
-            if((ai%2==0 && aj%2==0) || (ai%2!=0 && aj%2!=0)){
-                if (((ai%4==0 && (ai+2)==aj)||(aj%4==0 && (aj+2)==ai)) || ((ai%4==1 && (ai+2)==aj)||(aj%4==1 && (aj+2)==ai)) ){
-                    return;
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t=1; t<=T; t++){
+            int N = sc.nextInt();
+            LinkedHashMap<Long, Long> lhmap = new LinkedHashMap<Long, Long>();
+            long odd = 0;
+            long even = 0;
+            for(int i=1; i<=N; i++){
+                long element = sc.nextLong();
+                if(element%2==0){
+                    even++;
                 }
-                c++;
+                else{
+                    odd++;
+                }
+                if(lhmap.get(element) == null){
+                    long one = 1;
+                    lhmap.put(element, one);
+                }
+                else{
+                    long count = lhmap.get(element);
+                    lhmap.put(element, ++count);
+                }
             }
-            return;
+            long total = odd*(odd-1)/2 + even*(even-1)/2;
+            // System.out.println(total);
+            for (Map.Entry<Long, Long> m : lhmap.entrySet()){
+                long element = m.getKey();
+                long count = m.getValue();
+                long duplicate = count*(count-1)/2;
+                // System.out.println(duplicate+" of "+element);
+                total -= duplicate;
+                if((element%4==0 && lhmap.containsKey(element+2)) || (element%4==1 && lhmap.containsKey(element+2))){
+                    Long i = element+2;
+                    total -= count*lhmap.get(i);
+                    // System.out.println(element+" "+(element+2)+" ka xor2 hata "+count+" baar");
+                }
+            }
+            System.out.println(total);
         }
-        if (i >= n)
-            return;
-        comb[index] = arr[i];
-        combinations(arr, n, r, index+1, comb, i+1);
-        combinations(arr, n, r, index, comb, i+1);
     }
-    
 }
