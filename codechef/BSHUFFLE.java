b0f27e7dff4f6ad7852026a32aeb7f14e7d47b84
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-09-16 05:19:44 +0530

    September Long Problem 4

--- codechef/BSHUFFLE.java
+++ codechef/BSHUFFLE.java
@@ -7,92 +7,30 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
-    static HashMap<String, Integer> map = new HashMap<>();
-    static int max = 0, min = 0;
-    static String maxval, minval;
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		char arr[] = new char[N];
-		for (int i=0; i<N; i++){
-		    arr[i] = Integer.toString(i+1).charAt(0);
+		int arrmax[] = new int[N+1];
+		int arrmin[] = new int[N+1];
+		for (int i=1; i<=N; i++){
+		    arrmax[i] = i+1;
+		    arrmin[i] = i-1;
 		}
-// 		System.out.println("Initial Mappings are: " + map);
-		printAllKLength(arr, N);
-// 		System.out.println("Finally the map looks like this: " + map);
+		arrmax[N] = 1;
+		arrmin[1] = N;
 		
-        Map.Entry<String, Integer> minn = null;
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (minn == null || minn.getValue() > entry.getValue()) {
-                minn = entry;
-            }
-        }
-		minval = minn.getKey();
-		
-// 		System.out.println("Max Value: " + maxval + " with count= " + max);
-// 		System.out.println("Min Value: " + minval + " with count= " + minn);
-		
-		String maxprint = "", minprint = "";
-		for (int i=0; i < maxval.length(); i++){
-		    maxprint = maxprint + maxval.charAt(i) + " ";
-		    minprint = minprint + minval.charAt(i) + " ";
+	    int temp = arrmax[N];
+        arrmax[N] = arrmax[(N+1)/2];
+        arrmax[(N+1)/2] = temp;
+	
+		for (int i=1; i<=N; i++){
+		    System.out.print(arrmax[i]+" ");
 		}
-		maxprint.trim();
-		minprint.trim();
-		
-		System.out.println(maxprint);
-		System.out.println(minprint);
-		
+		System.out.println();
+		for (int i=1; i<=N; i++){
+		    System.out.print(arrmin[i]+" ");
+		}
 	}
-	
-    static void printAllKLength(char[] set, int k) 
-    { 
-        int n = set.length;  
-        printAllKLengthRec(set, "", n, k); 
-    } 
-
-    static void printAllKLengthRec(char[] set, String prefix, int n, int k) 
-    {
-        if (k == 0){
-            String finalprefix = randomizing(prefix);
-            Integer c = map.get(finalprefix); 
-            
-            if (map.get(finalprefix) == null) 
-                map.put(finalprefix, 1); 
-            else{
-                ++c;
-                if(max < c){
-                    max = c;
-                    maxval = finalprefix;
-                }
-                map.put(finalprefix, c);
-            }
-                
-            return; 
-        }
-        
-        for (int i = 0; i < n; ++i) {
-            String newPrefix = prefix + set[i];
-            printAllKLengthRec(set, newPrefix, n, k - 1);  
-        } 
-    }
-    
-    static String randomizing(String str){
-        char arr[] = new char[str.length()];
-        for (int i=0; i<arr.length; i++){
-		    arr[i] = Integer.toString(i+1).charAt(0);
-		}
-        for (int i=0; i<arr.length; i++){
-            char ch = str.charAt(i);
-            int pos = (int)ch - 49;
-            char temp = arr[i];
-            arr[i] = arr[pos];
-            arr[pos] = temp;
-        }
-        String newstr = new String(arr);
-        return newstr;
-    }
-	
 }
