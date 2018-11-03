18db265ce5d16f9129be29772a5ce8c80b1f5dad
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-11-03 19:55:53 +0530

    October Long Problem 2 (Partial Solution)

---
+++ codechef/BITOBYT.java
@@ -0,0 +1,40 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int t=1; t<=T; t++){
+		    int N = sc.nextInt();
+		    int i = 1;
+		    int bitt=1, nibbble=0, bytee=0;
+		    int count = 0;
+		    while(i<=N){
+		        if(count==2){
+		            nibbble = bitt;
+		            bitt = 0;
+		        }
+		        else if(count==10){
+		            bytee = nibbble;
+		            nibbble = 0;
+		        }
+		        else if(count==26){
+		            bitt = 2*bytee;
+		            bytee = 0;
+		            count = 0;
+		        }
+		        count++;
+		        i++;
+		    }
+		    System.out.println(bitt+" "+nibbble+" "+bytee);
+		} 
+	}
+}
